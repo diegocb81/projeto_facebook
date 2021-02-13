46a40a48add906cd06d7a19724683e6e8c8a9991
Diego Barcellos <diegocb81@gmail.com>
2021-02-13 19:15:45 -0300

    Adicionando compatibilidade do IE com o Edge

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <!--icon created by flaticon/freepik-->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=5.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <link rel="stylesheet" href="style.css">
 </head>
 
